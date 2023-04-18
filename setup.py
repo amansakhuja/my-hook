1f1a9fab5a74c4ccf64664ade1f38afaea79cc68
Aman Sakhuja <aman.sakhuja21@gmail.com>
2023-04-18 19:05:43 +0530

    WIP

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+
+setup(
+    extras_require={
+        "dev": [
+        ]
+    },
+    install_requires=[
+    ],
+    dependency_links=[], 
+    license="Apache License 2.0",
+    name="myhook",
+    version=version,
+    python_requires=">=3.7",
+    description="Infrastructure as code static analysis",
+    author="amansakhuja",
+    author_email="aman.sakhuja21@gmail.com",
+    packages=find_packages(exclude=["tests*", "integration_tests*"]),
+    url="https://www.python.org/sigs/distutils-sig/",
+    scripts=["bin/myhook"],
+)
